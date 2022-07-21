981c1a124e8ccceb342c2a365ebdc523da1ac6a2
Vijayalaxmi <Vijayalaxmi@LAPTOP-17BCPM31>
2022-07-21 14:37:00 +0530

    commit1

---
+++ base/src/test/java/base/abcTest.java
@@ -0,0 +1,19 @@
+package base;
+
+import org.testng.annotations.Test;
+
+@Test
+public class abcTest {
+	public void ab1() {
+		System.out.println("hello");
+	}
+	@Test
+	public void ab2() {
+		System.out.println("hello1");
+		
+	}
+	@Test
+	public void ab3() {
+		System.out.println("hello2");
+	}
+}
---
+++ base/src/test/java/base/defTest.java
@@ -0,0 +1,11 @@
+package base;
+
+public class defTest {
+	public void de1() {
+		System.out.println("Hi");
+	}
+	public void de2() {
+		System.out.println("Hi 1");
+	}
+
+}
